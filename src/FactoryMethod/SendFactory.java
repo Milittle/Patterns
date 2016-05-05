b638e19caf1b9e570abd7969229ac96123c2becb
Mi Zeshuang <mizeshuang@gmail.com>
2016-05-05 18:25:26 +0800

    alter SendFactory

--- src/FactoryMethod/SendFactory.java
+++ src/FactoryMethod/SendFactory.java
@@ -4,6 +4,8 @@ package FactoryMethod;
  * Created by mizeshuang on 2016/5/5.
  */
 public class SendFactory {
+
+    /*普通的工厂方法*/
     public Sender produce(String type) {
         if ("mail".equals(type)) {
             return new MailSender();
@@ -14,4 +16,15 @@ public class SendFactory {
             return null;
         }
     }
+
+    /*多个工厂方法*/
+    public Sender produceMail() {
+        return new MailSender();
+    }
+
+    public Sender produceSms() {
+        return new SmsSender();
+    }
+
+    /*也可以静态工厂方法*/
 }
