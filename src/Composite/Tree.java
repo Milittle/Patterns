ca160a5c623b0dab61f3cc3d20929777a8754115
Mi Zeshuang <mizeshuang@gmail.com>
2016-05-06 20:49:48 +0800

    add Composite

---
+++ src/Composite/Tree.java
@@ -0,0 +1,22 @@
+package Composite;
+
+/**
+ * Created by mizeshuang on 2016/5/6.
+ */
+public class Tree {
+    TreeNode root = null;
+
+    public Tree(String name) {
+        root = new TreeNode(name);
+    }
+
+    public static void main(String[] args) {
+        Tree tree = new Tree("A");
+        TreeNode node = new TreeNode("B");
+        TreeNode node1 = new TreeNode("c");
+
+        node.add(node1);
+        tree.root.add(node);
+        
+    }
+}
